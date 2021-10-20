10ba6fdbb56fdaceb13691148024980218f2ace1
Rohan Kiratsata <kiratsatarohan1234@gmail.com>
2021-10-20 21:55:57 +0530

    Added Skills Div

--- js/theme-toggle.js
+++ js/theme-toggle.js
@@ -1,4 +1,8 @@
 function toggleTheme(){
     var elem = document.body;
+
+    //var a = document.getElementsByClassName("bar");
+
+    // a.classList.toggle("dark-toggle");
     elem.classList.toggle("toggle-theme");
 }
