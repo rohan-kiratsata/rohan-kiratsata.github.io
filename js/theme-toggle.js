4db222fb1b682c6a98e37ddda174ff5b4eda30e6
Rohan Kiratsata <kiratsatarohan1234@gmail.com>
2021-11-01 19:13:25 +0530

    Changed Site

--- js/theme-toggle.js
+++
@@ -1,8 +0,0 @@
-function toggleTheme(){
-    var elem = document.body;
-
-    //var a = document.getElementsByClassName("bar");
-
-    // a.classList.toggle("dark-toggle");
-    elem.classList.toggle("toggle-theme");
-}
